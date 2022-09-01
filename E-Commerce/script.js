23917f2f7c8f9e990da86875ea5a8a127e053908
Sourav Choudhury <90979611+tesseract5633182@users.noreply.github.com>
2022-09-01 11:31:45 +0530

    First commit

---
+++ E-Commerce/script.js
@@ -0,0 +1,9 @@
+let mainImg = document.getElementById('mainImg')
+let sm_image = document.getElementsByClassName('sm-image')
+
+for (i=0; i<5;i++){
+        console.log(i)
+        
+}
+        
+        
